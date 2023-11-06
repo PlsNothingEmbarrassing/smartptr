c91a06c36206cc55777fcc25747bf88526c4fe4b
j5-hutton <jude2.hutton@live.uwe.ac.uk>
2023-11-06 16:37:43 +0000

    Add new file

---
+++ Task 4/my_string.h
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class my_string
+{
+private:
+    int length;
+    smart_ptr<char> ptr_;
+
+public:
+    // Defualt constructor
+    my_string() : ptr_(nullptr), length(0) {}
+    // Creating object with smart pointer class
+    my_string(const char *s) : ptr_(new char[std::strlen(s)]), length(0)
+    {
+        length = strlen(s);
+        strcpy(ptr_.get(), s);
+    }
+    // Copy constructor
+    my_string(const my_string &other) : ptr_(other.ptr_), length(other.length)
+    {
+    }
+    // Overloaded assignment operator
+    my_string &operator=(const my_string &other)
+    {
+        if (this != &other)
+        {
+            ptr_ = other.ptr_;
+            length = other.length;
+        }
+        return *this;
+    }
+    // Member fuctions
+    char getChar(const int &i) const
+    {
+        if (i >= 0 && i < length)
+        {
+            return ptr_.get()[i];
+        }
+        return '\0';
+    }
+
+    void setChar(const int &i, const char &c)
+    {
+        if (i >= 0 && i < length)
+        {
+            ptr_.get()[i] = c;
+        }
+    }
+
+    void print() const
+    {
+        if (ptr_.get())
+        {
+            std::cout << ptr_.get() << " "
+                      << "[" << ptr_.get_count() << "]" << std::endl;
+        }
+        else
+        {
+            std::cout << "(Empty String)" << std::endl;
+        }
+    }
+};
+
+class ReferenceCount
+{
+private:
+    // Where the actual count of the managed object is being stored
+    int RefCount{0};
+
+public:
+    void Increment()
+    {
+        ++RefCount;
+    }
+    int Decrement()
+    {
+        return --RefCount;
+    }
+    int GetCount() const
+    {
+        return RefCount;
+    }
+};
+
+template <typename T>
+class smart_ptr
+{
+private:
+    // Pointer to managed object
+    T *m_Object{nullptr};
+    // Pointer to managed object reference count
+    ReferenceCount *m_ReferenceCount{nullptr};
+
+public:
+    smart_ptr() {}
+    // Constructor
+    smart_ptr(T *object) : m_Object{object}, m_ReferenceCount{new ReferenceCount()}
+    {
+        m_ReferenceCount->Increment();
+        cout << "Created ptr ref count is " << m_ReferenceCount->GetCount() << endl;
+    }
+    // Virtual deconstructor - managed object and count is destroyed when ref count is 0
+    virtual ~smart_ptr()
+    {
+        if (m_ReferenceCount)
+        {
+            int decrementedCount = m_ReferenceCount->Decrement();
+            cout << "Destroyed ptr! Ref count is " << decrementedCount << endl;
+            if (decrementedCount <= 0)
+            {
+                delete m_ReferenceCount;
+                delete m_Object;
+                m_ReferenceCount = nullptr;
+                m_Object = nullptr;
+            }
+        }
+    }
+    // Copy Constructor
+    smart_ptr(const smart_ptr<T> &other) : m_Object{other.m_Object}, m_ReferenceCount{other.m_ReferenceCount}
+    {
+        m_ReferenceCount->Increment();
+        cout << "Copied ptr ref count is " << m_ReferenceCount->GetCount() << endl;
+    }
+
+    // Overloaded Assignment Operator
+    smart_ptr<T> &operator=(const smart_ptr<T> &other)
+    {
+        if (this != &other)
+        {
+            if (m_ReferenceCount && m_ReferenceCount->Decrement() == 0)
+            {
+                delete m_ReferenceCount;
+                delete m_Object;
+            }
+            m_Object = other.m_Object;
+            m_ReferenceCount = other.m_ReferenceCount;
+            m_ReferenceCount->Increment();
+        }
+        cout << "Assigning ptr ref count is " << m_ReferenceCount->GetCount() << endl;
+        return *this;
+    }
+    // Dereference operator
+    T &operator*()
+    {
+        return *m_Object;
+    }
+
+    T *operator->()
+    {
+        return m_Object;
+    }
+    // Member access to pointer
+    T *get() const
+    {
+        return m_Object;
+    }
+    int get_count() const
+    {
+        return m_ReferenceCount->GetCount();
+    }
+};
