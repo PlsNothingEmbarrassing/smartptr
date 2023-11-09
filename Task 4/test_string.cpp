97f1484a46ae640ec0546905954cefc2fbd2647b
j5-hutton <jude2.hutton@live.uwe.ac.uk>
2023-11-09 14:16:46 +0000

    Update test_string.cpp

--- Task 4/test_string.cpp
+++ Task 4/test_string.cpp
@@ -1,37 +1,48 @@
 #include <cstring>
 #include <iostream>
 #include "my_string.h"
-// TODO implement unit tests
-int main()
-{
-  my_string s("Hello world");
-  s.print();
-  s = "Hello";
-  s.print();
-  s = "Hello world";
+#include <gtest/gtest.h>
 
-  {
-    my_string t = s;
-    s.print(); // Reference count should be 2
-    t.print(); // Reference count should be 2
+// Test constructor
+TEST(MyStringTest, ConstructorTest) {
+    my_string str("Hello World");    
+    EXPECT_EQ(str.getChar(0), 'H');
+    EXPECT_EQ(str.get_count(), 1);
+}
+// Test default constructor for count = 0
+TEST(MyStringTest, DefConstructorTest) {
+    my_string str;    
+    EXPECT_EQ(str.getChar(0), '\0');
+    EXPECT_EQ(str.get_count(), 0);
+}
 
-    {
-      my_string u = s;
-      s.print(); // Reference count should be 3
-      t.print(); // Reference count should be 3
-      u.print(); // Reference count should be 3
-    }
+// Test copy constructor
+TEST(MyStringTest, CopyConstructorTest) {
+    my_string original("Hello, World");
+    my_string copy(original);
+    EXPECT_EQ(copy.getChar(0), 'H');
+    EXPECT_EQ(copy.get_count(), 2);
+}
 
-    s.print(); // Reference count should be 2
-    t.print(); // Reference count should be 2
-  }
+// Test assignment operator
+TEST(MyStringTest, AssignmentOperatorTest) {
+    my_string str1("Hello");
+    my_string str2("World");
+    str2 = str1;
+    EXPECT_EQ(str2.getChar(0), 'H');
+    EXPECT_EQ(str2.get_count(), 2);
+}
 
-  s.print(); // Reference count should be 1
-  std::cout << s.getChar(1) << std::endl;
+// Test setChar method
+TEST(MyStringTest, SetCharTest) {
+    my_string str("Hello");
+    str.setChar(1, 'i');
+    EXPECT_EQ(str.getChar(1), 'i');
+}
 
-  s.setChar(1, 'E');
-  s.print();
 
-  std::cout << "End of main" << std::endl;
-  return 0;
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
